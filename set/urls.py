454ad2b65d2fddf0ac7ffb0a25a782d389d3c6a0
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-01 19:36:05 +0200

    update set/urls.py

--- set/urls.py
+++ set/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import rest_framework.urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import UserViewSet, GroupViewSet
+
+router = DefaultRouter()
+router.register('user', UserViewSet)
+router.register('group', GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include(rest_framework.urls, namespace='rest_framework')),
 ]
