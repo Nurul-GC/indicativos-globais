ef3b6e04e132423e5b9b1c23959705bfc6f4574c
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-01 07:32:53 +0200

    update set/settings.py

--- set/settings.py
+++ set/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from os import path, mkdir
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 
@@ -27,7 +27,6 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,10 +74,13 @@ WSGI_APPLICATION = 'set.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+if not path.exists(f'{BASE_DIR}/database'):
+    mkdir(f'{BASE_DIR}/database')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database/indig.db',
     }
 }
 
