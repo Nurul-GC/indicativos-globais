a11f8fea1d9892a36f7ff8fac76d3dfafa713975
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-03 00:01:52 +0200

    update set/urls.py

--- set/urls.py
+++ set/urls.py
@@ -27,5 +27,5 @@ router.register('group', GroupViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include(rest_framework.urls, namespace='rest_framework')),
+    path('api-auth/', include(rest_framework.urls, namespace='rest_framework'))
 ]
