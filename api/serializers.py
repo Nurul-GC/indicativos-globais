f0c9fff7dda99cc0f129bb883f95c3937afddbbb
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-03 04:46:04 +0200

    update api/serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,14 @@
-from django.contrib.auth.models import User, Group
+from api.models import Indicativos, Paises
 from rest_framework.serializers import HyperlinkedModelSerializer
 
 
+class IndicativoSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Indicativos
+        fields = ['indicativo']
+
+
+class PaiseSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Paises
+        fields = ['pais']
