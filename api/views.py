82f45e40b60b774058710cece3f0838d6847b12b
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-03 02:56:03 +0200

    update api/views.py

--- api/views.py
+++ api/views.py
@@ -4,18 +4,6 @@ from api.serializers import UserSerializer, GroupSerializer
 
 
 # Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
 class IndicativosViewSet(viewsets.ModelViewSet):
     queryset = ''
     serializer_class = ''
