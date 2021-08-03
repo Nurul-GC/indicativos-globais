cfbf04efd87a0d4a82cdb420d863ae281c2cbd27
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-03 04:46:13 +0200

    update api/views.py

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, permissions
-from api.serializers import UserSerializer, GroupSerializer
+from rest_framework import viewsets
+
+from api.models import Indicativos, Paises
+from api.serializers import IndicativoSerializer, PaiseSerializer
 
 
 # Create your views here.
+class PaisesViewSet(viewsets.ModelViewSet):
+    queryset = Paises.objects.all().order_by('pais')
+    serializer_class = PaiseSerializer
+
+
 class IndicativosViewSet(viewsets.ModelViewSet):
-    queryset = ''
-    serializer_class = ''
-    permission_classes = ['']
+    queryset = Indicativos.objects.all()
+    serializer_class = IndicativoSerializer
