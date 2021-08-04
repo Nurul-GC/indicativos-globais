c1565995a68f20fb9b483660a91faf947452e2f2
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-04 23:37:30 +0200

    update herokulogs.py

--- herokulogs.py
+++ herokulogs.py
@@ -1,9 +1,10 @@
+from os import curdir
 from subprocess import getoutput
 from time import sleep
 
 if __name__ == '__main__':
     log = getoutput("heroku logs")
     sleep(0.5)
-    with open("etc/heroku_logs.txt", "+w") as log_file:
+    with open(f"{curdir}/extra/heroku_logs.txt", "+w") as log_file:
         log_file.write(log)
     print('done..')
