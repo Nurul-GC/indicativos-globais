cf325c440ec1d19ec222db4abc4e2f88274e4e70
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-04 08:08:28 +0200

    update api/views.py

--- api/views.py
+++ api/views.py
@@ -1,15 +1,11 @@
+from django.db import router
 from rest_framework import viewsets
 
-from api.models import Indicativos, Paises
-from api.serializers import IndicativoSerializer, PaiseSerializer
+from api.models import Indicativos
+from api.serializers import IndicativoSerializer
 
 
 # Create your views here.
-class PaisesViewSet(viewsets.ModelViewSet):
-    queryset = Paises.objects.all().order_by('pais')
-    serializer_class = PaiseSerializer
-
-
 class IndicativosViewSet(viewsets.ModelViewSet):
     queryset = Indicativos.objects.all()
     serializer_class = IndicativoSerializer
