9ef1863f73d0c5c024d3cb6bb098433ac61b105e
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-05 00:19:28 +0200

    update herokulogs.py

--- herokulogs.py
+++ herokulogs.py
@@ -5,6 +5,6 @@ from time import sleep
 if __name__ == '__main__':
     log = getoutput("heroku logs")
     sleep(0.5)
-    with open(f"{curdir}/extra/heroku_logs.txt", "+w") as log_file:
+    with open("heroku_logs.txt", "+w") as log_file:
         log_file.write(log)
     print('done..')
