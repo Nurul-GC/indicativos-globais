6644f943337ea1406e2bcbc426f4522c64800fa1
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-04 23:24:42 +0200

    update herokulogs.py

--- extra/herokulogs.py
+++
@@ -1,8 +0,0 @@
-from subprocess import getoutput
-from os import chdir
-
-chdir('..')
-log = getoutput('heroku logs --tail')
-with open('etc/heroku_logs.txt', '+w') as log_file:
-    log_file.write(log)
-    print('done..')
---
+++ herokulogs.py
@@ -0,0 +1,9 @@
+from subprocess import getoutput
+from time import sleep
+
+if __name__ == '__main__':
+    log = getoutput("heroku logs")
+    sleep(0.5)
+    with open("etc/heroku_logs.txt", "+w") as log_file:
+        log_file.write(log)
+    print('done..')
