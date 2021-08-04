6bc304d2be68c4058b3ac8d5998688e175ffec37
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-04 08:08:04 +0200

    update api/models.py

--- api/models.py
+++ api/models.py
@@ -4,21 +4,14 @@ from api.indicativos import GCI
 
 
 # Create your models here.
-class Paises(models.Model):
+class Indicativos(models.Model):
     PAISES = []
+    INDICATIVOS = None
     for p in GCI().paises():
-        PAISES.append((p, p))
+        INDICATIVOS = GCI().indicativo_especifico(_pais=p)
+        PAISES.append((INDICATIVOS, p))
 
     pais = models.CharField(max_length=50, choices=PAISES)
 
     def __str__(self):
         return self.pais
-
-
-class Indicativos(models.Model):
-    pais = models.ForeignKey(Paises, on_delete=models.CASCADE)
-    indi_esp = GCI().indicativo_especifico(_pais=pais.name)
-    indicativo = models.CharField(max_length=10, default=indi_esp, editable=False)
-
-    def __str__(self):
-        return self.indicativo
