16dd75f1200d4880b88c69b15c183547961adf47
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-04 08:08:19 +0200

    update api/serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,9 @@
-from api.models import Indicativos, Paises
 from rest_framework.serializers import HyperlinkedModelSerializer
+
+from api.models import Indicativos
 
 
 class IndicativoSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Indicativos
-        fields = ['indicativo']
-
-
-class PaiseSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = Paises
-        fields = ['pais']
+        fields = ['id', 'pais']
