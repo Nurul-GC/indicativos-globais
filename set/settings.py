3aca53c567747a17cb53ef9ac1e97f10fad3e16b
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-08 16:40:28 +0200

    update set/settings.py

--- set/settings.py
+++ set/settings.py
@@ -99,12 +99,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Rest definitions
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
-}
-
 # Heroku definitions
 settings(locals())
 TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
