3d4bfa8a198e41b542edc51bc2161f16229e6765
Nurul-GC <nuruldecarvalho@gmail.com>
2021-08-08 16:23:42 +0200

    update indig/views.py

--- indig/views.py
+++ indig/views.py
@@ -5,5 +5,4 @@ from indig.serializers import IndicativoSerializer
 
 # Create your views here.
 class IndicativosViewSet(viewsets.ModelViewSet):
-    queryset = Indicativos.objects.all()
     serializer_class = IndicativoSerializer
