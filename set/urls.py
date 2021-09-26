4c25e1a5728240c2c00e69cae0dc2b2f54ec501a
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-26 03:46:45 +0200

    update set/urls.py

--- set/urls.py
+++ set/urls.py
@@ -2,13 +2,14 @@ import rest_framework.urls
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from indig.views import IndicativosViewSet
+from indig.views import IndicativosViewSet, index
 
 router = DefaultRouter()
 router.register('indicativos', IndicativosViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', index, name='index'),
+    path('api/', include(router.urls, namespace='indicativos')),
     path('master/', admin.site.urls),
     path('indig-auth/', include(rest_framework.urls, namespace='rest_framework'))
 ]
