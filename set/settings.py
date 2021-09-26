a66d9bed5c9dc4476da8b8b48ffb9a42d98957f1
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-26 23:57:30 +0200

    update set/settings.py

--- set/settings.py
+++ set/settings.py
@@ -103,7 +103,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = f'{BASE_DIR}/static/'
+STATIC_URL = 'static/'
 
 # Heroku definitions
 settings(locals())
