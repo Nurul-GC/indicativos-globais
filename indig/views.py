907152fb2a3d5cb86551c6e8ce7d8ef68c067572
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-26 03:47:36 +0200

    update indig/views.py

--- indig/views.py
+++ indig/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from indig.models import Indicativos
 from indig.serializers import IndicativoSerializer
@@ -7,3 +8,7 @@ from indig.serializers import IndicativoSerializer
 class IndicativosViewSet(viewsets.ModelViewSet):
     queryset = Indicativos.objects.all()
     serializer_class = IndicativoSerializer
+
+
+def index(request):
+    render(request, 'index.html')
