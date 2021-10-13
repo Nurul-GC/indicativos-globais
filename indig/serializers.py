d77124731c5dd6c385d762b7088b09a95ab59ad1
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-13 18:39:19 +0200

    update project

--- indig/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from indig.models import Indicativos
-
-
-class IndicativoSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = Indicativos
-        fields = ['id', 'pais']
